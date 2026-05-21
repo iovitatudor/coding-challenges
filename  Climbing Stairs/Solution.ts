4c471bea31adcda66f8d42b8bf4b5f24e50cc6ce
Tudor Iovita <iovitatudor21@gmail.com>
2026-05-21 19:18:41 +0300

    Add Climbing Stairs ts version

---
+++  Climbing Stairs/Solution.ts
@@ -0,0 +1,19 @@
+function climbStairs(n: number): number {
+
+    if (n <= 2) {
+        return n;
+    }
+
+    let first = 1;
+    let second = 2;
+
+    for (let i = 3; i <= n; i++) {
+
+        const temp = first + second;
+
+        first = second;
+        second = temp;
+    }
+
+    return second;
+}
