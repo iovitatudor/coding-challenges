4597a3541a867221e748222de2dc833066b99e0a
Tudor Iovita <iovitatudor21@gmail.com>
2026-05-24 19:43:15 +0300

    Add Same Tree php version

---
+++ Same Tree/Solution.php
@@ -0,0 +1,45 @@
+<?php
+
+// Definition for a binary tree node.
+
+class TreeNode
+{
+    public $val = null;
+    public $left = null;
+    public $right = null;
+
+    function __construct($val = 0, $left = null, $right = null)
+    {
+        $this->val = $val;
+        $this->left = $left;
+        $this->right = $right;
+    }
+}
+
+class Solution
+{
+
+    /**
+     * @param TreeNode $p
+     * @param TreeNode $q
+     * @return Boolean
+     */
+    function isSameTree(TreeNode $p, TreeNode $q): bool
+    {
+        if($p === null && $q === null) {
+            return true;
+        }
+
+        if($p === null || $q === null) {
+            return false;
+        }
+
+        if($p->val !== $q->val) {
+            return false;
+        }
+
+        return
+            $this->isSameTree($p->left, $q->left) &&
+            $this->isSameTree($p->right, $q->right);
+    }
+}
