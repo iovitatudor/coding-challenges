9165843ab8863c71591275eaa60e1980e7db7176
Tudor Iovita <iovitatudor21@gmail.com>
2026-08-01 19:25:08 +0300

    Update Finding Purchases task setup and solution

--- SQL/Level_2/Window_Functions/Ranking_Most_Active_Guests/solution.sql
+++ SQL/Level_2/Window_Functions/Ranking_Most_Active_Guests/solution.sql
@@ -1,12 +1,20 @@
-WITH ordered_guests AS (
-    SELECT  id_guest,
-            SUM(n_messages) AS sum_n_messages
-    FROM airbnb_contacts
-    GROUP BY id_guest
-)
+-- WITH ordered_guests AS (
+--     SELECT  id_guest,
+--             SUM(n_messages) AS sum_n_messages
+--     FROM airbnb_contacts
+--     GROUP BY id_guest
+-- )
+--
+-- SELECT DENSE_RANK() OVER(ORDER BY sum_n_messages DESC) AS ranking,
+--     id_guest,
+--        sum_n_messages
+-- FROM ordered_guests
+-- ORDER BY ranking ASC;
 
-SELECT DENSE_RANK() OVER(ORDER BY sum_n_messages DESC) AS ranking,
+SELECT
+    DENSE_RANK() OVER(ORDER BY sum(n_messages) DESC) as ranking,
     id_guest,
-       sum_n_messages
-FROM ordered_guests
-ORDER BY ranking ASC;
+    sum(n_messages) as sum_n_messages
+FROM airbnb_contacts
+GROUP BY id_guest
+ORDER BY sum_n_messages DESC
