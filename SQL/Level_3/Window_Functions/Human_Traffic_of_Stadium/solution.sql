27cba47b2c2d24fb2e76cba0e7d14f58fdea3023
Tudor Iovita <iovitatudor21@gmail.com>
2026-08-18 18:24:09 +0300

    add Human Traffic of Stadium task

---
+++ SQL/Level_3/Window_Functions/Human_Traffic_of_Stadium/solution.sql
@@ -0,0 +1,15 @@
+WITH FIlTRED AS (SELECT id,
+                        visit_date,
+                        people,
+                        id - ROW_NUMBER() OVER(ORDER BY id) as grp
+                 FROM Stadium
+                 WHERE people >= 100)
+SELECT id,
+       visit_date,
+       people
+FROM FIlTRED
+WHERE grp IN (SELECT grp
+              FROM FIlTRED
+              GROUP BY grp
+              HAVING COUNT(*) > 3)
+ORDER BY visit_date ASC;
