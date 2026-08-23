c06646de4c533538333189b6e7654b61441810ec
Tudor Iovita <iovitatudor21@gmail.com>
2026-08-23 19:01:10 +0300

    add Monthly Transactions I task

---
+++ SQL/Level_2/Aggregations_and_Grouping/Monthly_Transactions_I/solution.sql
@@ -0,0 +1,8 @@
+SELECT TO_CHAR(trans_date, 'YYYY-MM') AS month,
+        country,
+        COUNT(*) AS trans_count,
+        SUM(CASE WHEN state = 'approved' THEN 1 ELSE 0 END) AS approved_count,
+        SUM(amount) AS trans_total_amount,
+        COALESCE(SUM(CASE WHEN state = 'approved' THEN amount END), 0) AS approved_total_amount
+FROM Transactions
+GROUP BY TO_CHAR(trans_date, 'YYYY-MM'), country
