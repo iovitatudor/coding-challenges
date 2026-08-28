8f663f9e75ae0d0ba43d165cf83108595f3145bd
Tudor Iovita <iovitatudor21@gmail.com>
2026-08-28 18:19:31 +0300

    add Sales Person task

---
+++ SQL/Level_1/Joins_and_Date_Filtering/Sales_Person/solution.sql
@@ -0,0 +1,8 @@
+SELECT sp.name AS name
+FROM SalesPerson AS sp
+WHERE NOT EXISTS (SELECT 1
+                  FROM Orders AS o
+                           JOIN Company AS c
+                                ON c.com_id = o.com_id
+                  WHERE o.sales_id = sp.sales_id
+                    AND c.name = 'RED')
