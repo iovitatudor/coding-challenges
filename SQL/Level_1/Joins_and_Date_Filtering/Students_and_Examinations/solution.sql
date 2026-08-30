2406ba3640b6f4f457aaa55d6436f940ae031c03
Tudor Iovita <iovitatudor21@gmail.com>
2026-08-30 18:23:47 +0300

    add Students and Examinations task

---
+++ SQL/Level_1/Joins_and_Date_Filtering/Students_and_Examinations/solution.sql
@@ -0,0 +1,13 @@
+SELECT s.student_id,
+       s.student_name,
+       sb.subject_name,
+       COUNT(e.subject_name) AS attended_exams
+FROM Students AS s
+         CROSS JOIN Subjects AS sb
+         LEFT JOIN Examinations AS e
+                   ON s.student_id = e.student_id
+                       AND sb.subject_name = e.subject_name
+GROUP BY s.student_id,
+         s.student_name,
+         sb.subject_name
+ORDER BY s.student_id, sb.subject_name;
