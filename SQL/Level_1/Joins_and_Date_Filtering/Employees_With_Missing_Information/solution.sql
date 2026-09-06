d1c93bfc4dfb6d21846fe04ef3cad51ec4a1c7d9
Tudor Iovita <iovitatudor21@gmail.com>
2026-09-06 19:04:12 +0300

    add Employees With Missing Information task

---
+++ SQL/Level_1/Joins_and_Date_Filtering/Employees_With_Missing_Information/solution.sql
@@ -0,0 +1,7 @@
+SELECT COALESCE(e.employee_id, s.employee_id) AS employee_id
+FROM Employees AS e
+         FULL JOIN Salaries AS s
+                   ON e.employee_id = s.employee_id
+WHERE e.name IS NULL
+   OR s.salary IS NULL
+ORDER BY COALESCE(e.employee_id, s.employee_id);
