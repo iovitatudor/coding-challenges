7408a70ab6346e9667a2847b5cf5ea162d97e035
Tudor Iovita <iovitatudor21@gmail.com>
2026-09-20 18:28:00 +0300

    add Find Books with No Available Copies task

---
+++ SQL/Level_2/Aggregations_and_Grouping/Find_Books_with_No_Available_Copies/solution.sql
@@ -0,0 +1,19 @@
+SELECT lb.book_id,
+       lb.title,
+       lb.author,
+       lb.genre,
+       lb.publication_year,
+       COUNT(br.record_id) AS current_borrowers
+FROM library_books AS lb
+         INNER JOIN borrowing_records AS br
+                    ON lb.book_id = br.book_id
+WHERE return_date IS NULL
+GROUP BY lb.book_id,
+         lb.title,
+         lb.author,
+         lb.genre,
+         lb.publication_year,
+         lb.total_copies
+HAVING COUNT(br.record_id) = lb.total_copies
+ORDER BY current_borrowers DESC,
+         lb.title ASC;
